8cb239b1936c6f608ef1888886e609480f370c30
rodolfomayora <rodolfomayora96@gmail.com>
2021-08-13 18:19:45 -0400

    feat: add Layout component

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LayoutStyled = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  min-height: 100vh;
+  padding: 28px 0;
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Board } from './Board';
+export { default as Layout } from './Layout';
+export { default as ResetButton } from './ResetButton';
 export { default as Tile } from './Tile';
+export { default as WordLabel } from './WordLabel';
